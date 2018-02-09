21836dc36f5d96d9dd3da25295626bfc9b3439db
Richard Swanson <rswanson05@gmail.com>
2018-02-09 14:56:13 -0600

    AutoHueLights turns on lights when phone is detected and off lights when phone is not detected, next need to account for when user turns off lights

--- AutoHueLights.sh
+++ AutoHueLights.sh
@@ -11,20 +11,51 @@ DetectPhone()
 		sudo hcitool cc $1 2>/dev/null
 
 		bt="$(hcitool rssi $1 | grep -o [0-9]*)"
-		bt=$(($bt+0))
-		((PingStatus+=$bt))
-		if [ "$bt">=0 ]; then
-			end=$(($(date +%s%N)/1000000))
-			echo "$bt", "$PingStatus"
+		if [ -z "$bt" ]; then
+			echo "NotConnected", "$NotConnected"
+			NotConnected=$(($NotConnected+1))
 		else
-			echo "NotConnected","$bt"
-			NotConnected=$(($NotConnected+0))
+			bt=$(($bt+0))
+			((PingStatus+=$bt))
+			echo "$bt", "$PingStatus"
 		fi
 	}
+
+	if [ $PingStatus -le 5 ] && [ $NotConnected -lt 2 ]; then
+		PhoneStatus=1
+	elif [ $PingStatus -ge 95 ] || [ $NotConnected -ge 2 ]; then
+		PhoneStatus=0
+	fi
 }
 
+GetLightStatus()
+{
+	LightsResponse="$(curl -H "Accept: application/json" -X GET http://192.168.99.27/api/TupfOmPqTaiMkujwG-nwUzScxnC3Chubz9ys3Q5I/groups/1/ | grep -oP '("all_on":).{0,4}')" >/dev/null
+	if [ "$LightsResponse" == "\"all_on\":true" ]; then
+		LightStatus=1
+	elif [ "$LightsResponse" == "\"all_on\":fals" ]; then
+		LightStatus=0
+	else
+		LightStatus=-1
+		echo $LightsResponse
+	fi
+}
 
 echo "START"
+while true; do
 DetectPhone $RichardPhoneMAC
-echo "END"
+echo PhoneStatus $PhoneStatus
+GetLightStatus
+echo LightStatus $LightStatus
+
+if [ $LightStatus == 0 ] && [ $PhoneStatus == 1 ]; then
+	echo ON
+	curl -H "Accept: application/json" -X PUT --data '{"on":true}' http://192.168.99.27/api/TupfOmPqTaiMkujwG-nwUzScxnC3Chubz9ys3Q5I/groups/1/action > /dev/null
+elif [ $LightStatus == 1 ] && [ $PhoneStatus == 0 ]; then
+	echo OFF
+	curl -H "Accept: application/json" -X PUT --data '{"on":false}' http://192.168.99.27/api/TupfOmPqTaiMkujwG-nwUzScxnC3Chubz9ys3Q5I/groups/1/action > /dev/null
+fi
+sleep 0.5
+done
+
 
