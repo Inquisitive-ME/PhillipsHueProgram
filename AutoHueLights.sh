4065c2db9f66be06949fa8ad403c57f06c4fe508
root <rswanson05@gmail.com>
2018-02-09 19:46:56 -0600

    Working version of Auto Hue Lights, need to work more on detecting home and away

--- AutoHueLights.sh
+++ AutoHueLights.sh
@@ -7,9 +7,10 @@ DetectPhone()
 	echo $1
 	PingStatus=0
 	NotConnected=0
+	pings=0
 	for ((i=1;i <= 5;i++)){
 		sudo hcitool cc $1 2>/dev/null
-
+		sleep 0.5
 		bt="$(hcitool rssi $1 | grep -o [0-9]*)"
 		if [ -z "$bt" ]; then
 			echo "NotConnected", "$NotConnected"
@@ -17,13 +18,17 @@ DetectPhone()
 		else
 			bt=$(($bt+0))
 			((PingStatus+=$bt))
+			((pings+=1))
 			echo "$bt", "$PingStatus"
 		fi
 	}
-
-	if [ $PingStatus -le 5 ] && [ $NotConnected -lt 2 ]; then
+	if [ "$pings" == "0" ]; then
+		pings=1
+	fi
+	#echo test $(($PingStatus / $pings))
+	if [ $(($PingStatus/$pings)) -le 2 ] && [ $NotConnected -le 2 ]; then
 		PhoneStatus=1
-	elif [ $PingStatus -ge 95 ] || [ $NotConnected -ge 2 ]; then
+	elif [ $(($PingStatus/$pings)) -ge 12 ] || [ $NotConnected -ge 5 ]; then
 		PhoneStatus=0
 	fi
 }
@@ -42,20 +47,39 @@ GetLightStatus()
 }
 
 echo "START"
+GetLightStatus
+previousLightStatus=$LightStatus
+manual = 0
 while true; do
 DetectPhone $RichardPhoneMAC
 echo PhoneStatus $PhoneStatus
 GetLightStatus
 echo LightStatus $LightStatus
 
-if [ $LightStatus == 0 ] && [ $PhoneStatus == 1 ]; then
+if [ "$LightStatus" != "$previousLightStatus" ]; then
+	manual=1
+fi
+
+echo manual $manual
+
+if [ "$manual" == "1" ] && [ "$PhoneStatus" == "1" ]; then
+	previousLightStatus=$LightStatus
+elif [ "$manual" == "1" ] && [ "$PhoneStatus" == "0" ]; then
+	echo change manual
+	manual=0
+	curl -H "Accept: application/json" -X PUT --data '{"on":false}' http://192.168.99.27/api/TupfOmPqTaiMkujwG-nwUzScxnC3Chubz9ys3Q5I/groups/1/action > /dev/null
+	previousLightStatus=0
+elif [ "$LightStatus" == "0" ] && [ "$PhoneStatus" == "1" ]; then
 	echo ON
 	curl -H "Accept: application/json" -X PUT --data '{"on":true}' http://192.168.99.27/api/TupfOmPqTaiMkujwG-nwUzScxnC3Chubz9ys3Q5I/groups/1/action > /dev/null
-elif [ $LightStatus == 1 ] && [ $PhoneStatus == 0 ]; then
+	previousLightStatus=1
+elif [ "$LightStatus" == "1" ] && [ "$PhoneStatus" == "0" ]; then
 	echo OFF
 	curl -H "Accept: application/json" -X PUT --data '{"on":false}' http://192.168.99.27/api/TupfOmPqTaiMkujwG-nwUzScxnC3Chubz9ys3Q5I/groups/1/action > /dev/null
+	previousLightStatus=0
+else
+	previousLightStatus=$LightStatus
 fi
-sleep 0.5
+
+sleep 1
 done
-
-
